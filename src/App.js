ba105fb0595c6cb4d28d5b1abef2cca36e669fad
nik frank <nikbfrank@gmail.com>
2019-07-04 06:35:49 -0700

    check endgame

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     word: words[0],
     goodLetters: [],
     badLetters: [],
+    didWin: null,
   }
 
   componentDidMount(){
@@ -24,18 +25,49 @@ class App extends React.Component {
     });
   }
 
+  newGame = (didWin)=> {
+    this.setState({ didWin });
+
+    setTimeout(()=> this.setState({
+      word: words[ Math.floor( Math.random()*words.length ) ],
+      goodLetters: [],
+      badLetters: [],
+      didWin: null
+    }), 2000);
+  }
+
   guess = letter => {
     if( /[a-z]/.test(letter) ){
 
       if( this.state.word.includes(letter) &&
           !this.state.goodLetters.includes(letter) ){
+
         this.setState({
           goodLetters: [...this.state.goodLetters, letter],
+        }, ()=> {
+          let lettersLeft = this.state.word.replace(/ /g, '');
+
+          for(let i=0; i< this.state.goodLetters.length; i++){
+            lettersLeft = lettersLeft.split(this.state.goodLetters[i])
+                                     .join('');
+          }
+
+          if(!lettersLeft) {
+            console.log('YOU WON THE GAME SON');
+            this.newGame(!!'won');
+          }
         });
+
       } else if( !this.state.word.includes(letter) &&
                  !this.state.badLetters.includes(letter) ){
         this.setState({
           badLetters: [...this.state.badLetters, letter],
+        }, ()=> {
+          console.log('bad guess', this.state);
+          if(this.state.badLetters.length === 7){
+            console.log('YOU LOST THE GAME FOU');
+            this.newGame(!'won');
+          }
         });
       }
     }
@@ -44,6 +76,19 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
+        {
+          this.state.didWin ? (
+            <div className='winner'><span>
+              WINNER WINNER CHICKEN DINNER
+            </span></div>
+          ) :
+          this.state.didWin !== null ? (
+            <div className='loser'><span>
+            you're dead loser.
+          </span></div>
+          ) :
+          null
+        }
         <Gallow strikes={this.state.badLetters.length}/>
         <div className='word'>
           {
